7f6373e30844b97318d1ae192560dbc266bcf685
krezket <125393906+krezket@users.noreply.github.com>
2023-05-08 20:09:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helper });
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.SESSION_SECRET
   cookie: {
     maxAge:1000*60*60*2
   },
